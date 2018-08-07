eadcc2500ce4cc81c1b1042452933a239cd52df9
injuryepi <injury.epi@gmail.com>
2018-08-06 17:37:21 -0700

    Add files via upload

--- data-raw/icd10cm_intent_mech_regex.r
+++ data-raw/icd10cm_intent_mech_regex.r
@@ -7,6 +7,6 @@ icd10cm_intent_mech_regex <- icd10cm_intent_mech_regex %>%
   mutate(mechanism = clean_mech_names(mechanism))
 
 icd10cm_intent_mech_regex <- icd10cm_intent_mech_regex %>%
-  mutate(intent_mechanism = paste(intent, mechanism, sep = "-"))
+  mutate(intent_mechanism = paste(intent, mechanism, sep = "_"))
 
 devtools::use_data(icd10cm_intent_mech_regex, compress = "xz", overwrite = T)
